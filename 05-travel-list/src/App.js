cdba8ba123d11f2a2844d103537c782471a55890
eswar <m.venkatapraveen@mvenkatapraveens-MacBook-Air.local>
2023-08-10 13:11:05 +0530

    05 Concept :- Building the components

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,34 @@
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <Logo></Logo>
+      <Form></Form>
+      <PackingList></PackingList>
+      <Stats></Stats>
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for you trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
